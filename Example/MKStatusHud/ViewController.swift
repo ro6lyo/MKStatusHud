2837fbebb9b6f569a8c26cd5306f49f0712fe6ca
Mehmed Kadir <Mehmed.Kadir@bulpros.com>
2017-11-21 11:01:55 +0200

    Initial commit

---
+++ Example/MKStatusHud/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MKStatusHud
+//
+//  Created by ro6lyo on 11/21/2017.
+//  Copyright (c) 2017 ro6lyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
