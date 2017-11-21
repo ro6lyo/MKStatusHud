f5756fb144c1dc8806b110283bb33a6564669dd4
Mehmed Kadir <Mehmed.Kadir@bulpros.com>
2017-11-21 11:27:13 +0200

    version increase

--- Example/MKStatusHud/ViewController.swift
+++ Example/MKStatusHud/ViewController.swift
@@ -2,23 +2,105 @@
 //  ViewController.swift
 //  MKStatusHud
 //
-//  Created by ro6lyo on 11/21/2017.
-//  Copyright (c) 2017 ro6lyo. All rights reserved.
+//  Created by Mehmed Kadir on 11/08/2017.
+//  Copyright (c) 2017 Mehmed Kadir. All rights reserved.
 //
 
 import UIKit
+import MKStatusHud
+import Lottie
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        let hud = HUD(withImage: #imageLiteral(resourceName: "progress"), title: "Please wait!", subtitle: "While download proccess completes.")
+        hud.presentOnView = self.view
+        hud.rotation = true
+        hud.show(begin: {(isStarted) in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                hud.update(withImage: #imageLiteral(resourceName: "download"), title: "Done!", subtitle: "Download completed.",rotation: false,dismiss:2)
+            })
+        }) { (finished) in
+            print(finished)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func completeAnimeWithSuccess(_ sender: UIButton) {
+        //Create the hud
+        let hud = HUD(withAnimation: LOTAnimationView(name: "loader-success-failed"), title: "Please wait!", subtitle: "While download proccess completes.")
+        hud.animateToProgress = 0.275
+        hud.animationLoop = true
+        
+        //Present the hud
+        hud.show(begin: { (isStarted) in
+            //...//
+            // Some time lataer when download finished succesfully
+            //...//
+            self.succesFinish(hud)
+            //Optional completion for progress time
+        }, progress: { (progress) in
+            //Optional completion for animation finish
+        }, animation: { (isFinished) in
+            if isFinished {
+                hud.updateWith(title: "Done!", subtitle: "Process completed.")
+                hud.close(hideAfter: 2)
+            }
+            //Optional completion for hud dismissal
+        }, completion: { (completed) in
+            print("hud dissmissed - > \(completed)")
+        })
     }
-
+    
+    @IBAction func completeAnimeWithError(_ sender: UIButton) {
+        let hud = HUD(withAnimation: LOTAnimationView(name: "loader-success-failed"), title: "Please wait!", subtitle: "While download proccess completes.")
+        hud.animateToProgress = 0.275
+        hud.animationLoop = true
+        
+        hud.show(begin: { (isStarted) in
+            self.errorFinish(hud)
+        }, progress: { (progress) in
+        }, animation: { (isFinished) in
+            if isFinished {
+                hud.updateWith(title: "Sorry!", subtitle: "Try again later.")
+                hud.close(hideAfter: 2)
+            }
+        }, completion: { (completed) in
+            print("hud dissmissed - > \(completed)")
+        })
+    }
+    
+    @IBAction func loadingAction(_ sender: Any) {
+        let hud = HUD(withAnimation: LOTAnimationView(name: "dna_like_loader"), title: "Please wait!", subtitle: "While download proccess completes.")
+        hud.animationLoop = true
+        
+        hud.show(begin: { (isStarted) in
+            //Some time later close the hud
+            hud.close(hideAfter: 10)
+            
+        }, progress: { (progress) in
+        }, animation: { (isFinished) in
+        }, completion: { (completed) in
+            print("hud dissmissed - > \(completed)")
+        })
+        
+    }
+    
+    
+    private func succesFinish(_ hud:HUD) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+            hud.animateToProgress = 0.47
+            hud.animationLoop = false
+            hud.update(withAnimation: nil, title: "Finishing up", subtitle: "Will be ready in a second.")
+        })
+    }
+    
+    private func errorFinish(_ hud:HUD) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5, execute: {
+            hud.animateFromProgresPlus = 0.5
+            hud.animateToProgress = 0.95
+            hud.animationLoop = false
+            hud.update(withAnimation: nil, title: "It semms like", subtitle: "Taking to much time.")
+        })
+    }
 }
 
